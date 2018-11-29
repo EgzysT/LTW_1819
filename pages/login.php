7faed3b3a7ee73590af0a3c2588d553bfeeee9d1
anasasilva <anasasousasilva@gmail.com>
2018-11-29 17:40:55 +0000

    Adding login page

--- pages/login.php
+++ pages/login.php
@@ -1,13 +1,12 @@
 <?php 
   // include_once('../includes/session.php');
   include_once('../templates/tpl_common.php');
-  // include_once('../templates/tpl_auth.php');
+  include_once('../templates/tpl_auth.php');
 
   // Verify if user is logged in
   if (isset($_SESSION['username']))
     die(header('Location: list.php'));
 
   draw_header(NULL);
-  // draw_login();
-  // draw_footer();
+  draw_login();
 ?>
