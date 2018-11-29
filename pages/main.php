2cc4000693f90bbf6eab0b2f41caa93d999e2327
anasasilva <anasasousasilva@gmail.com>
2018-11-29 19:24:34 +0000

    fixing include bug

--- pages/main.php
+++ pages/main.php
@@ -1,7 +1,7 @@
 <?php 
   include_once('../includes/session.php');
   include_once('../templates/tpl_common.php');
-  include_once('../templates/tpl_story_card.php');
+  include_once('../templates/tpl_story_cards.php');
 
   draw_header(null);
   draw_footer();
