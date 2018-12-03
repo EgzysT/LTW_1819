c33336bbdc0233d8c17b2ae1997f9efe14c1faf6
Fábio Oliveira <fabio_the_boss@hotmail.com>
2018-12-03 15:31:02 +0000

    Added subscribe/unsubscribe button 3drotation animations.

--- templates/tpl_asides.php
+++ templates/tpl_asides.php
@@ -8,10 +8,10 @@
         <h3 class="aside-header-text">Meaningful Stories</h3>
         <p class="aside-body-text">Stories worth reading about life and technology. Crafted with pen and passion by our community.</p>
         <footer>
-            <button class="button button-green"> Green Button </button>
-            <button class="button button-red"> Red Button </button>
-            <button class="button button-blue"> Search </button>
-            <button class="button button-orange"> Something Else </button>
+            <button class="button button-green button-channel-subscription button-180Y-rotate" id="subscribe"> Subscribe </button>
+            <button class="button button-red button-channel-subscription" id="unsubscribe"> Unsubscribe </button>
+            <!--<button class="button button-blue"> Search </button>
+            <button class="button button-orange"> Something Else </button>-->
         </footer>
     </aside>
 
