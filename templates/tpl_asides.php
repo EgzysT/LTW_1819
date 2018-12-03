125cdd67576759749076a757de0bc7005eb63977
Fábio Oliveira <fabio_the_boss@hotmail.com>
2018-12-03 14:48:50 +0000

    Added new colors for buttons. Refactored CSS

--- templates/tpl_asides.php
+++ templates/tpl_asides.php
@@ -8,7 +8,10 @@
         <h3 class="aside-header-text">Meaningful Stories</h3>
         <p class="aside-body-text">Stories worth reading about life and technology. Crafted with pen and passion by our community.</p>
         <footer>
-            <button class="button button-blue search-button"> Search </button>
+            <button class="button button-green"> Green Button </button>
+            <button class="button button-red"> Red Button </button>
+            <button class="button button-blue"> Search </button>
+            <button class="button button-orange"> Something Else </button>
         </footer>
     </aside>
 
