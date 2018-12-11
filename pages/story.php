b5d2a5dcdb67a558dcc1b1d8614240e93355a6ee
anasasilva <anasasousasilva@gmail.com>
2018-12-11 15:11:31 +0000

    fixed story card

--- pages/story.php
+++ pages/story.php
@@ -51,7 +51,7 @@
   	<section id="one-story" class="one-story">
       <?php
           draw_aside_profile($username, $profile_pic, $bio, $points);
-          draw_full_story_card($current_story, $vote_type);
+          draw_full_story_card($current_story, $comments, $vote_type);
           draw_comments($comments, $story_id, $logged_in);
       ?>
 	  </section>
